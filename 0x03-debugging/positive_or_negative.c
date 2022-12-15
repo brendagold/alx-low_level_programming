428fb640c9258b0a0dee985d739bc723eb6df8bc
brendagold <petersbrenda47@gmail.com>
2022-12-15 13:50:20 -0800

    added compilation of task 0

--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -3,13 +3,13 @@
 #include <time.h>
 
 /**
- * main - Entry point
+ * positive_or_negative - Entry point
  *
  * Return: Always 0 (Success)
  *
  */
 
-int main(void)
+void positive_or_negative(void)
 {
 	int n;
 
@@ -31,7 +31,5 @@ int main(void)
 		printf("%d is zero\n", n);
 
 	}
-
-	return (0);
 
 }
