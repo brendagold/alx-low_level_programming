bf8d79b400624ebd8614a095e994b68a5ad46fc8
brendagold <petersbrenda47@gmail.com>
2022-12-15 17:10:06 -0800

    added advanced tasks

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * print_number - print number
+ *
+ * @n: parameter
+ *
+ * Return: 0 if successful
+ */
+
+void print_number(int n)
+{
+	unsigned int i, j, count;
+
+	if (n < 0)
+	{
+		_putchar(45);
+		i = n * -1;
+	}
+	else
+		i = n;
+
+	j = i;
+	count = 1;
+
+	while (j > 9)
+	{
+		j /= 10;
+		count *= 10;
+	}
+
+	for (; count >= 1; count /= 10)
+	{
+		_putchar(((i / count) % 10) + 48);
+	}
+}
