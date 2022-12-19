12c8edcbb9ec34d74648ad1da81782df0f928c87
brendagold <petersbrenda47@gmail.com>
2022-12-19 13:11:19 -0800

    fixed tasks 4 and 5

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -12,7 +12,7 @@ void print_rev(char *s)
 	int i = 0;
 	int j;
 
-	while (str[i] != '\0')
+	while (s[i] != '\0')
 	{
 		i++;
 	}
--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -15,8 +15,8 @@ void rev_string(char *s)
 
 	for (i = 0; i < _strlen(s) / 2; i++)
 	{
-		t = s[i];
+		temp = s[i];
 		s[i] = s[_strlen(s) - i - 1];
-		s[_strlen(s) - i - 1] = t;
+		s[_strlen(s) - i - 1] = temp;
 	}
 }
