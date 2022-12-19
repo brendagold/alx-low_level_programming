4e3e87374ed107a3ab58be6d800aad075942a34d
brendagold <petersbrenda47@gmail.com>
2022-12-19 13:05:16 -0800

    done task 2 to 5

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_rev - This function prints a string in reverse order
+ * @s: pointer value
+ * Return: Always 0 (Success)
+ */
+
+void print_rev(char *s)
+{
+	int i = 0;
+	int j;
+
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+	for (j = i - 1; j >= 0; j--)
+	{
+		_putchar(s[j]);
+	}
+
+	_putchar('\n');
+}
---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+#include "2-strlen.c"
+
+/**
+ * rev_string - reverse a string value
+ * @s: Pointer parameter
+ * Return: Always 0 (Success)
+ */
+
+void rev_string(char *s)
+{
+	int i;
+	char temp;
+
+	for (i = 0; i < _strlen(s) / 2; i++)
+	{
+		t = s[i];
+		s[i] = s[_strlen(s) - i - 1];
+		s[_strlen(s) - i - 1] = t;
+	}
+}
